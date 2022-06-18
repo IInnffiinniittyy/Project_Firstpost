6ace302bced76fe834924b405ba6a2a5ec0115d8
vinaybarve <vinaybarve40@gmail.com>
2022-06-18 10:55:51 +0530

    adding files

---
+++ politics/politics.js
@@ -0,0 +1,176 @@
+// we white here politics.js
+
+
+
+
+
+
+let dataSy =[
+
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/large_file_plugin/2020/09/1599937556_1552291928_SukhbirSinghBadal_AFP.jpg?impolicy=website&width=640&height=363",
+      h1tag:"Shiromani Akali Dal reveals 64 of its 97 candidates in Punjab Assembly Election 2022",
+      ptag:"Though the party had earlier announced names of 24 candidates, it released a complete list of 64 candidates on Monday for the 2022 state assembly polls",
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/01/BJP-flag.jpg?impolicy=website&width=640&height=363",
+      h1tag:"Centre's move to recruit 10 lakh employees: BJP hails announcement, Opposition dubs it as 'jumle-baazi'",
+      ptag:"Congress leader Rahul Gandhi, however, took a swipe at the Centre over the announcement, saying this is a government of not 'jumlas' but 'maha jumlas New Delhi: The BJP on Tuesday hailed Prime Minister Narendra Modi's directive to various government departments and ministries to undertake recruitment of 10 lakh people in the next year and a half,"
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2021/08/bhupesh-baghel-nbm-2.jpg?impolicy=website&width=640&height=362",
+      h1tag:"Chattisgarh: Bhupesh Baghel accuses Centre of misusing agencies to suppress Opposition",
+      ptag:"The ED on Friday issued a fresh summons to Sonia Gandhi asking her to appear on 23 June for questioning in connection with the National Herald money laundering case Raipur: Chhattisgarh Chief Minister Bhupesh Baghel on Sunday alleged that the Central government is misusing agencies like Enforcement Directorate (ED), Central Bureau Investigation (CBI) and Income Tax to suppress the voice of the opposition"
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/Nadda1.jpg?impolicy=website&width=640&height=363",
+      h1tag:"Bengal BJP's infighting comes to the fore amid JP Nadda's visit",
+      ptag:"According to state BJP sources, several party MLAs were not present at the state executive committee meeting held at the National Library and addressed by Nadda Kolkata: In the midst of the ongoing visit by BJP national president J P Nadda the infighting in the party's West Bengal unit came out in the open on Wednesday with its vice-president Raju Banerjee claiming he was not invited to the state executive meeting. "
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/TKS-Elangovan-2.jpg?impolicy=website&width=640&height=362",
+      h1tag:"DMK leader defends his casteist comment 'Hindi will reduce Tamils to status of 'shudras'",
+      ptag:"TKS Elangovan also claimed that Hindi-speaking states in the country were not developed, while those with vernacular languages as mother tongue were doing well Hours after causing a storm with his remark on Hindi, DMK leader and the party's Rajya Sabha member TKS Elangovan on Monday defended his comments by saying the arrival of the language from the north has divided people in south, according to a report by Times Now."
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/large_file_plugin/2020/11/1606734684_KSEshwarappapti.jpeg?impolicy=website&width=640&height=363",
+      h1tag:"AAP demands Karnataka BJP leader Eshwarappa's arrest for his national flag remark",
+      ptag:"BJP MLA Eshwarappa has reportedly said the saffron flag has been respected for thousands of years and it will become the national flag one day New Delhi: AAP leader Sanjay Singh has submitted a complaint to police against the BJP's K Eshwarappa over his reported remarks that the saffron flag would become the national flag of India one day and said he should be arrested."
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/pawar-mamata.jpg?impolicy=website&width=640&height=363",
+      h1tag:"Explained: Who will be the Opposition candidate for Presidential polls? Is Sharad Pawar out?",
+      ptag:"Nationalist Congress Party chief Sharad Pawar has the backing of the Congress and the Trinamool to join the race to Rashtrapati Bhavan. But looks like the veteran leader has other plans. What are the Opposition’s other choices?"
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/Amit-Shah-meeting11.jpg?impolicy=website&width=640&height=362",
+      h1tag:"Agnipath Scheme: Agniveers will be co-opted in CAPF, Assam Rifles, says Home Ministry",
+      ptag:"The Agnipath Scheme launched by PM Narendra Modi is a visionary step for ensuring a bright future for the country’s youth, Shah said on Twitter The Union Home Ministry on Wednesday announced that preference will be given to Agniveers, the products of the Agnipath Scheme, in various CAPFs and Assam Rifles at the end of their 4-year tenure."
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/Rahul-Gandhi-1.jpg?impolicy=website&width=640&height=362",
+      h1tag:"National Herald case: Enforcement Directorate asks Rahul Gandhi to rejoin probe for third consecutive day",
+      ptag:"Rahul Gandhi is being questioned in detail about the ownership of Young Indian Private Limited (YIL) by the Gandhi family and its shareholding pattern in Associated Journals Limited (AJL), the company that runs the National Herald newspaper"
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/aaditya2.jpg?impolicy=website&width=640&height=362",
+      h1tag:"Aaditya’s moment in the sun: How Ayodhya visit marks coming of age for Thackeray scion",
+      ptag:"Aaditya Thackeray will be in Ayodhya for a day-long trip, which will see him doing a darshan and an aarti at River Sarayu. The timing of the visit is significant, as it comes ahead of the Mumbai civic body polls and amid the attack on Shiv Sena over the Hindutva issue After being in his grandfather and father’s shadow, the moment is finally here. Aaditya Thackeray, Maharashtra tourism minister and Shiv Sena scion, will make his way to Ayodhya today on his first solo trip."
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/Lawrence-Bishnoi11.jpg?impolicy=website&width=640&height=363",
+      h1tag:"Sidhu Moose Wala murder case: Gangster Lawrence Bishnoi remanded to seven-day police custody",
+      ptag:"A Delhi court on Tuesday granted Punjab Police the transit remand of Bishnoi to take him to Punjab in the case related to the singer Sidhu Moose Wala's death Chandigarh: Punjab Police brought gangster Lawrence Bishnoi under heavy security to the state from Delhi in the early hours of Wednesday to question him in the singer Sidhu Moosewala murder case."
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/05/Sonia-Gandhi-and-Rahul-Gandhi1.jpg?impolicy=website&width=640&height=363",
+      h1tag:"National Herald case: Rahul Gandhi, his mother and the Al Capone moment",
+      ptag:"Delhi logged 622 fresh COVID-19 cases and two deaths on Thursday, while the positivity rate rose to 3.17 per cent"
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/ajit-pawar1.jpg?impolicy=website&width=640&height=363",
+      h1tag:"Not allowing Deputy CM Ajit Pawar to speak at PM event an insult to Maharashtra: NCP",
+      ptag:"The prime minister inaugurated a shila temple at the Sant Tukaram Maharaj Mandir dedicated to the 17th-century saint in Dehu Mumbai: NCP leader Supriya Sule on Tuesday said Maharashtra Deputy Chief Minister Ajit Pawar was denied permission by the PMO to speak in the presence of Prime Minister Narendra Modi at a temple inauguration event in Dehu near Pune city, and dubbed it as an insult to the state."
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/large_file_plugin/2020/09/1600001265_sitaram-yechury-pti-875.jpg?impolicy=website&width=640&height=363",
+      h1tag:"'Lakhs of jobs lying vacant for eight years': Sitaram Yechury questions Centre's job announcement ",
+      ptag:"Earlier today, the prime minister's office announced that the recruitment of 10 lakh people will be done by the government in a mission mode in the next 1.5 years New Delhi: Raising questions over the Centre's announcement of providing 10 lakh jobs in the next 18 months, Communist Party of India (Marxist) General Secretary Sitaram Yechury on Tuesday asked why the government did not fill the lakhs of government jobs lying vacant during the last eight years."
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/Kerala-CM11.jpg?impolicy=website&width=640&height=363",
+      h1tag:"IndiGo investigating incident of sloganeering by Youth Congress workers on its flight",
+      ptag:"The airline is investigating the incident under DGCA's 'unruly flyer' regulations, which allow the airline concerned to deem a passenger's behaviour as unruly and ban the individual from flight for a specific period of time"
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/arrest14.jpg?impolicy=website&width=640&height=363",
+      h1tag:"Jharkhand: BJP leader arrested for 'objectionable' Facebook post in support of Nupur Sharma",
+      ptag:"Anisha Sinha, an executive committee member of the BJP's district committee, was arrested on Monday Adityapur: A local BJP leader was arrested in Jharkhand's Seraikela-Kharswan district for an objectionable Facebook post in support of the party's now-suspended spokesperson Nupur Sharma, police said on Tuesday."
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/fvmbekcwyaapdqe.jpg?impolicy=website&width=640&height=363",
+      h1tag:"Watch: Congress' Srinivas dragged into police bus as he dramatically tries to escape",
+      ptag:"Congress party on Tuesday said the Centre is scared of the Wayanad MP as he raises his voice against issues like unemployment, inflation and Chinese infiltration New Delhi: Indian Youth Congress president Srinivas BV was detained by Delhi Police on Tuesday while he was protesting over Enforcement Directorate (ED) probe against party leader Rahul Gandhi in the National Herald case."
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2018/08/kolkata-high-court.jpg?impolicy=website&width=640&height=363",
+      h1tag:"Calcutta HC asks CBI to probe 'illegal' appointments of primary teachers in Bengal schools",
+      ptag:"Justice Gangopadhyay directed the central agency to institute a case and start an investigation forthwith into the alleged illegal appointments to primary schoolscKolkata: The Calcutta High Court has directed the CBI to look into the appointments of 269 primary school teachers in West Bengal government-sponsored and -aided schools over allegations that they did not pass the eligibility test."
+
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/Rahul-Gandhi.jpg?impolicy=website&width=640&height=363",
+      h1tag:"National Herald case: After 10 hours of questioning yesterday, Rahul Gandhi appears before ED again today",
+      ptag:"Gandhi had arrived at the ED’s office on Monday with his sister and Congress general secretary Priyanka Gandhi Vadra, amid a huge march by the party workers Congress leader Rahul Gandhi appeared before the ED on Tuesday for the second consecutive day of questioning in the National Herald money-laundering case."
+
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2020/07/chidambaram-pti-640.jpg?impolicy=website&width=640&height=363",
+      h1tag:"'P Chidambaram suffered rib injury after Delhi police manhandling,' alleges Congress",
+      ptag:"The Congress party organised a 'Satyagrah' march against the government and in support of Gandhi across the country in which many party leaders were detained during the day by the police New Delhi: Senior Congress leader and former Union Minister P Chidambaram suffered a fracture in his left rib after he was pushed away by Delhi Police during the party's protest in support of Rahul Gandhi who was summoned by the Enforcement Directorate in connection with the National Herald case, said the party on Monday."
+
+    },
+]
+
+
+
+let chaildSy="chilSy"
+let link= "./health.html"
+
+dataSy.forEach(function (el, index, arr) {
+
+    let avtar = document.createElement("img")
+
+    avtar.setAttribute("src",el.photo)
+
+    let imdiv=document.createElement("div")
+    imdiv.setAttribute("class" , chaildSy)
+
+    let A=document.createElement("a")
+    A.setAttribute("href" , link)
+    let h1=document.createElement("h1")
+    let p=document.createElement("p")
+   
+    h1.innerText = el.h1tag
+    p.innerText = el.ptag
+
+    // h3.innerText=""
+    A.innerText="HEALTH"
+
+    
+    imdiv.append(A,h1,p)
+    document.querySelector("#mainSy").append(avtar,imdiv)
+
+
+})
+
+
+
+
+console.log(document)
