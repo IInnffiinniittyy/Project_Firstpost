5cf013a11b5f4459f9827b92139559327b8ddc88
vinaybarve <vinaybarve40@gmail.com>
2022-06-18 09:53:29 +0530

    addong cricket news

---
+++ cricket - Copy/cricket.js
@@ -0,0 +1,176 @@
+// we white here politics.js
+
+
+
+
+
+
+let dataSy =[
+
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/Ruturaj-640_Sportzpics.jpg?impolicy=website&width=640&height=363",
+      h1tag:"India vs South Africa: Life is not on edge, IPL performance has increased expectations, says Ruturaj Gaikwad",
+      ptag:"Gaikwad has amassed 1207 runs in 36 IPL matches so far but managed just 120 in six T20Is, including the maiden fifty that he scored in the third T20 against South Africa. Visakhapatnam: He hasn't been able to translate his IPL form into national colours but India opener Ruturaj Gaikwad is not too worried as for him it is all about staying consistent mentally and trusting the process",
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/Temba-Bavuma-vs-IND-Sportzpics-640.jpg?impolicy=website&width=640&height=363",
+      h1tag:"India vs South Africa: Would be a bit foolish to change batting approach after just one loss, says Bavuma",
+      ptag:"Chasing 180 to win, South Africa had a slow start as they could manage only 15 runs in the first three overs but Bavuma said it 'has always been our tactic as a team. Visakhapatnam: South Africa's batting crumbled under pressure in the third T20I against India but skipper Temba Bavuma said it would be'"
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/StokesBairstow-min.jpg?impolicy=website&width=640&height=362",
+      h1tag:"Watch: Jonny Bairstow-Ben Stokes help England smash 160 runs in 16 overs to script record Test chase",
+      ptag:"It was truly the beginning of a new era for England under Brendon McCullum and Ben Stokes as a belligerent performance by their batters saw them chase down 299 runs in 50 overs on the final day of the second Test against New Zealand. Despite losing three wickets early in the chase, England never took their foot off the pedal and Jonny Bairstow and Ben Stokes did not slow down even once in the chase. "
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/Kyle-jamieson.jpg?impolicy=website&width=640&height=363",
+      h1tag:"England vs New Zealand: Black Caps pacer Kyle Jamieson to return home with back injury",
+      ptag:"New Zealand management confirmed Jamieson will return home after a scan revealed a stress reaction in his lower back, suffered while bowling on the third day of the second Test. Nottingham: New Zealand will be without injured pace bowler Kyle Jamieson for the third and final Test against England, adding to their gloom after conceding the series in Nottingham on Tuesday. "
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/HardikPandyaL.jpg?impolicy=website&width=640&height=363",
+      h1tag:"Former India cricketer’s tweet on Hardik Pandya wins Twitter",
+      ptag:"Amit Mishra said Hardik Pandya could well have flown a plane with low fuel and faulty engine in a thunderstorm and could have still managed to land it safely.Former India leg-spinner Amit Mishra has been a vocal presence on social media and he comes up with hilarious takes and good analysis post every India match. In the third T20I, India were powered by a late flourish by Hardik Pandya as the hosts posted 179 in their allotted 20 overs."
+
+      
+    },
+    { 
+      photo:"https://www.firstpost.com/wp-content/uploads/2022/06/HarshalPatel-min.jpg?impolicy=website&width=640&height=362",
+      h1tag:"Watch: Harshal Patel bamboozles David Miller with a slower deliver",
+      ptag:"David Miller has been the man in form for South Africa and Harshal Patel landed a slower ball on a length and Miller was far too early into his stroke.It was a do-or-die match for India – they had lost their first couple of matches and had to win the third T20I against South Africa in Visakhapatnam to stay afloat in the series."
+
+      
+    },
+    { 
+      photo:"https://www.firstpost.com/wp-content/uploads/2022/06/Heinrich-Klaasen-IND-SA-Sportzpics-640.jpg",
+      h1tag:"India vs South Africa: Proteas go 2-0 up in T20I series with comfortable four-wicket win",
+      ptag:"South Africa restricted India to 148/6 after opting to field, before a fiery knock from Heinrich Klaasen (81) helped them cross the finish line with more than an over to spare."
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/Roland-Salai.jpg?impolicy=website&width=640&height=363",
+      h1tag:"Nations League: England humiliated by Hungary 4-0; Germany hammer Italy 5-2",
+      ptag:"England suffered their heaviest home defeat in nearly 100 years on Tuesday as Gareth Southgate's side went down 4-0 to Hungary in the UEFA Nations League, while it was also a night to forget for Italy as they lost 5-2 in Germany Paris: England suffered their heaviest home defeat in nearly 100 years on Tuesday as Gareth Southgate's side went down 4-0 to Hungary in the UEFA Nations League, while it was also a night to forget for Italy as they lost 5-2 in Germany."
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2021/09/Daniil-Medvedev-USO-AP-640.jpg?impolicy=website&width=640&height=363",
+      h1tag:"US Open lets Russian tennis players in after Wimbledon ban",
+      ptag:"Athletes from Russia and Belarus will play at Flushing Meadows under a neutral flag, confirmed the US Open organisers The US Open will allow tennis players from Russia and Belarus to compete this year despite the ongoing war in Ukraine, which prompted Wimbledon to ban those athletes. Doha: Australia on Tuesday celebrated the antics of dancing goalkeeper Andrew Redmayne whose penalty shootout save against Peru saw them to a fifth straight World Cup finals."
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/Australia-celebrate-Peru-shootout-FIFA-World-Cup-Qualifiers-AP-640.jpg?impolicy=website&width=640&height=363",
+      h1tag:"FIFA World Cup 2022 Qualifiers: Dancing goalkeeper Redmayne confirms Australia’s fifth consecutive appearance",
+      ptag:"The 33-year-old clowned his way along the goal line as Peru's players prepared to take their penalty kicks and his block of Alex Valera's shot gave Australia a 5-4 win after the two sides had been deadlocked without a goal for 120 minutes."
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/Mauricio.jpg?impolicy=website&width=640&height=363",
+      h1tag:"Mauricio Pochettino on brink of PSG departure: Reports",
+      ptag:"Zinedine Zidane has been strongly linked with succeeding Pochettino in the dugout at the Parc des Princes. Paris Saint-Germain are close to confirming the sacking of coach Mauricio Pochettino, according to French media reports late on Tuesday."
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/costa-rica.jpg?impolicy=website&width=640&height=362",
+      h1tag:"Costa Rica claim last FIFA World Cup 2022 place with victory over New Zealand",
+      ptag:"Joel Campbell's early goal gave the Central Americans a nervy 1-0 win over New Zealand in the play-off on Tuesday Doha: Costa Rica became the last team to qualify for this year's World Cup as Joel Campbell's early goal gave the Central Americans a nervy 1-0 win over New Zealand in a play-off on Tuesday. "
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/Chahal-vs-SA-Vizag-Sportzpics-640.jpg?impolicy=website&width=640&height=363",
+      h1tag:"Yuzvendra Chahal, Harshal Patel shine as India thump South Africa in 3rd T20I to stay alive",
+      ptag:"Then, Harshal, who returned his T20 best figures of 4-25, and Chahal 3-20, shared seven wickets to bowl out South Africa for 131, as India bounced back from their opening two losses."
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/Seth-Rollins-AJ-Styles-WWE-Raw-640.jpg?impolicy=website&width=640&height=363",
+      h1tag:"WWE Raw Results: Seth Rollins defeats AJ Styles to qualify for MITB; Veer Mahaan defeats Rey Mysterio ",
+      ptag:"Here's a roundup of the 13 June edition of Monday Night Raw that took place at the Intrust Bank Arena in Wichita, Kansas. Among the key takeaways from this week's edition of Monday Night Raw was Seth Freakin Rollins refusal to accept any blame for attacking Cody Rhodes, who had defeated him for a third consecutive time at Hell in a Cell last Sunday."
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/Monica-Puig-Rio-2016-gold-AP-640.jpg?impolicy=website&width=640&height=363",
+      h1tag:"‘My body had enough’: Rio Olympics gold medallist Monica Puig announces retirement from tennis aged 28",
+      ptag:"Puig, who became the first Puerto Rican to win an Olympic gold five years ago, made the announcement in a post on social media. Rio de Janeiro Olympics gold medalist Monica Puig announced her retirement from professional tennis at age 28 via a post on social media Monday."
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/serena-williams.jpg?impolicy=website&width=640&height=363",
+      h1tag:"Serena Williams confirms Wimbledon 2022 participation"
+      
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2021/08/Neeraj-Chopra-mens-javelin-final-Tokyo-2020-AP-640.jpg?impolicy=website&width=640&height=363",
+      h1tag:"Neeraj Chopra returns to action in highly-competitive field at Paavo Nurmi Games",
+      ptag:"Neeraj Chopra will be in action for the first time since his Tokyo Olympics gold medal. India's star athlete will be up against some of his toughest in-form rivals in Finnish city Turku.Olympic champion Neeraj Chopora returns to competitive action for the first time since his Tokyo Olympics triumph as he competes in the Paavo Nurmi Games in Finnish city Turku on Tuesday night. "
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/India-celebrate-Cambodia-ACQ-Twitter-@IndianFootball-640.jpg?impolicy=website&width=640&height=363",
+      h1tag:"India qualify for AFC Asian Cup 2023 after Palestine thrash Philippines 4-0",
+      ptag:"Palestine hammered Philippines 4-0 in the Group B third round clash in Ulaanbaatar, Mongolia, thereby sealing India's place in the main event that takes place next year. India confirmed their place in the finals of the 2023 AFC Asian Cup hours before kickoff in their final qualifying game against Hong Kong, courtesy Palestine's 4-0 thumping of Philippines in Ulaanbaatar, Mongolia on Tuesday."
+
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2021/08/Roger-Federer-640.jpg?impolicy=website&width=640&height=362",
+      h1tag:"Watch: Fan shows his tattoo to Roger Federer, here's how the Swiss tennis legend reacted",
+      ptag:"Roger Federer has been out of the game since his quarter-final defeat at Wimbledon almost one year ago. The tennis legend recently underwent his third knee surgery in 18 months.Roger Federer may not be visible on the courts these days, but the Swiss legend is still adored by legions of tennis fans. A video shared by the ATP recently has shown why the 40-year-old remains loved by people."
+
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/06/Stephen-Curry-NBA-Finals-Game-5-AP-640.jpg?impolicy=website&width=640&height=363",
+      h1tag:"NBA Finals: However it ends, the Golden State Warriors vs Boston Celtics showdown may be a classic",
+      ptag:"But this much is already clear: Even without a single game decided by single digits through the first five contests, these 2022 NBA Finals are on the brink of going down as a classic.The series won’t end until Thursday, or maybe Sunday. A champion will be crowned, an NBA Finals MVP trophy will be hoisted, one side will spray a few gallons of champagne and the other side will head out into a summer of lament."
+
+    },
+]
+
+
+
+let chaildSy="chilSy"
+let link= "./health.html"
+
+dataSy.forEach(function (el, index, arr) {
+
+    let avtar = document.createElement("img")
+
+    avtar.setAttribute("src",el.photo)
+
+    let imdiv=document.createElement("div")
+    imdiv.setAttribute("class" , chaildSy)
+
+    let A=document.createElement("a")
+    A.setAttribute("href" , link)
+    let h1=document.createElement("h1")
+    let p=document.createElement("p")
+   
+    h1.innerText = el.h1tag
+    p.innerText = el.ptag
+
+    // h3.innerText=""
+    A.innerText="HEALTH"
+
+    
+    imdiv.append(A,h1,p)
+    document.querySelector("#mainSy").append(avtar,imdiv)
+
+
+})
+
+
+
+
+console.log(document)
