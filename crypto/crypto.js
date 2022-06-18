cd4f416fdac7d7d5bd6c5606efbbe1e1cc8723dd
vinaybarve <vinaybarve40@gmail.com>
2022-06-18 11:39:31 +0530

    adding files

---
+++ crypto/crypto.js
@@ -0,0 +1,177 @@
+// we white here politics.js
+
+
+
+
+
+
+let dataSy =[
+
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2022/03/Crypto-AFP.jpg?impolicy=website&width=640&height=363",
+      h1tag:"Cryptos can lead to dollarisation of economy: RBI officials to par panel",
+      ptag:"There are an estimated 15 million to 20 million crypto investors in India, with total crypto holdings of around USD 5.34 billion New Delhi: Cryptocurrencies can lead to dollarisation of a part of the economy which would be against India's sovereign interest, top officials of the RBI have told a parliamentary panel, according to sources. ",
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2021/11/24bitcoin.jpg?impolicy=website&width=640&height=363",
+      h1tag:"Bitcoin plunges below $30,000, lowest since July 2021",
+      ptag:"The world's largest digital token fell as much as 3.1 per cent to $29,992 in Asian trading. Ether fell as much as 3.7 per cent, while Solana dropped 8.2 per cent and Avalanche dipped 10.4 per cent Bitcoin extended losses, dropping below $30,000 for the first time since July 2021, putting its decline from a November record high to more than 55% amid a global flight from riskier investments."
+    },
+    { 
+      photo:"https://www.firstpost.com/wp-content/uploads/2021/11/cybercrime-1.jpg?impolicy=website&width=640&height=362",
+      h1tag:"How a job scam revealed Rs 3,000 crore money laundering via cryptocurrencies",
+      ptag:"Money from this racket has already been siphoned off to countries like China and the Philippines using cryptocurrency wallets like Binance, Smart Cont, OK Coin, etc "
+    },
+    { 
+      photo:"https://www.firstpost.com/wp-content/uploads/2022/04/Untitled-design11.jpg?impolicy=website&width=640&height=362",
+      h1tag:"Biggest risk of cryptocurrency for all countries will be money laundering, it being used for financing terror: Sitharaman",
+      ptag:"Finance Minister Nirmala Sitharaman on Tuesday participated in a high-level panel discussion on 'Money at a Crossroad' hosted by Kristalina Georgieva, Managing Director, IMF"
+    },
+    { 
+      photo:"https://www.firstpost.com/wp-content/uploads/2022/04/Supreme-Court15.jpg?impolicy=website&width=640&height=362",
+      h1tag:"'This is not a Tis Hazari Court', SC chides crypto scam accused",
+      ptag:"The comment comes after non-compliance of its earlier order by a crypto-currency scam accused who did not share his username/passwords with the Enforcement Directorate"
+
+      
+    },
+    { 
+      photo:"https://www.firstpost.com/wp-content/uploads/2021/12/twitter-1.jpg?impolicy=website&width=640&height=362",
+      h1tag:"PM Narendra Modi's personal Twitter handle 'very briefly compromised', later secured: PMO",
+      ptag:"A tweet claiming that India has officially adopted bitcoin as legal tender and bought 500 BTC and is distributing them among its residents was put out from it"
+
+      
+    },
+    { 
+      photo:"https://www.firstpost.com/wp-content/uploads/2021/11/24bitcoin.jpg?impolicy=website&width=640&height=362",
+      h1tag:"Crypto-curious corporations struggle to find right recipe",
+      ptag:"Analysts say the total market value of cryptocurrencies has now topped $2 trillion, roughly half of which is bitcoin"
+
+      
+    },
+    { 
+      photo:"https://www.firstpost.com/wp-content/uploads/2022/03/Crypto-AFP.jpg?impolicy=website&width=640&height=362",
+      h1tag:"Hackers steal over $600 million in one of the biggest cryptocurrency heists",
+      ptag:"The haul was valued at $545 million when it was stolen on 23 March, but was worth about $615 million based on prices Tuesday, making it one of largest thefts ever in the crypto world."
+
+      
+    },
+    { 
+      photo:"https://www.firstpost.com/wp-content/uploads/2022/03/daler.jpg?impolicy=website&width=640&height=362",
+      h1tag:"Daler Mehndi buys ‘Balle Balle Land’ in Metaverse: Understanding what are virtual lands and why they are selling at a premium",
+      ptag:"The noted singer became the first Indian to buy land in Metaverse. He follows in the footsteps of celebrities like Snoop Dogg and famous companies such as Samsung, Nike and Adidas who have invested in the virtual space"
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2019/08/BITCOIN_1280_REUTERS11.jpg?impolicy=website&width=640&height=362",
+      h1tag:"Lok Sabha approves 'crypto tax' amendments; 'detrimental to investor growth', says industry expert",
+      ptag:"Under the amended law, loss from crypto assets cannot be set off against gains in crypto assets as well"
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2019/08/BITCOIN_1280_REUTERS11.jpg?impolicy=website&width=640&height=362",
+      h1tag:"Mauricio Pochettino on brink of PSG departure: Reports",
+      ptag:"Zinedine Zidane has been strongly linked with succeeding Pochettino in the dugout at the Parc des Princes. Paris Saint-Germain are close to confirming the sacking of coach Mauricio Pochettino, according to French media reports late on Tuesday."
+
+      
+    },
+    { 
+      photo:"https://www.firstpost.com/wp-content/uploads/2022/03/Joe-Biden-cryptocurrency-AP-640.jpg?impolicy=website&width=640&height=362",
+      h1tag:"US president Joe Biden signs executive order on cryptocurrency as its use explodes",
+      ptag:"Under the executive order, Biden also directed the Treasury Department and other federal agencies to study the impact of cryptocurrency on financial stability and national security. "
+
+      
+    },
+    { 
+      photo:"https://www.firstpost.com/wp-content/uploads/2022/03/Russia-Ukraine-currency-AP-640.jpg?impolicy=website&width=640&height=362",
+      h1tag:"Russia eyes sanctions workarounds in energy, gold, cryptocurrency",
+      ptag:"Experts expect Russia to try to mitigate the impact of the financial penalties by relying on energy sales and leaning on the country's reserves in gold and Chinese currency."
+
+      
+    },
+    { 
+      photo:"https://www.firstpost.com/wp-content/uploads/2022/02/Union-Budget-Nirmala-Sitharaman_edit.jpg?impolicy=website&width=640&height=362",
+      h1tag:"Government has sovereign right to tax profit made from cryptocurrency transactions, says finance minister ",
+      ptag:"Nirmala Sitharaman said 'banning or not banning cryptocurrency would come subsequently' while answering questions posed about the Union Budget in Rajya Sabha"
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2021/06/cryptocurrency-.jpg?impolicy=website&width=640&height=362",
+      h1tag:"Next year's I-T return to have separate column for crypto income, says Revenue Secretary Tarun Bajaj",
+      ptag:"Bajaj said gains from cryptocurrencies were always taxable and what the Budget proposed is not a new tax but providing certainty over the issue"
+
+      
+    },
+    { 
+      photo:"https://www.firstpost.com/wp-content/uploads/2021/11/24bitcoin.jpg?impolicy=website&width=640&height=362",
+      h1tag:"Crypto assests are being taxed at 30%, but like cocaine they are not legal yet",
+      ptag:"Now that the government has decided to tax these tokens, founders of the so-called crypto exchanges are again going to the market, falsely claiming their tokens have been recognised, legitimised"
+      
+
+      
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2021/03/Bitcoin-Ether-Cryptocurrency-min.jpg?impolicy=website&width=640&height=362",
+      h1tag:"Budget 2022: Crypto sector hopeful of regulatory approval as Centre introduces tax on digital assets",
+      ptag:"Finance minister Nirmala Sitharaman has proposed 30 per cent tax on income generated at the sale of any digital asset, even as the regulatory structure on them remains unclear "
+
+      
+    },
+    { 
+      photo:"https://www.firstpost.com/wp-content/uploads/2022/02/old-people-india-1.jpg?impolicy=website&width=640&height=362",
+      h1tag:"Union Budget 2022: Set up separate ministry, initiate special digital literacy scheme for elderly, recommend NGOs",
+      ptag:"HelpAge India, a not-for-profit working for elder care, sought measures in three core areas — income and social security, health and geriatric care, and creating an enabling environment for elders"
+
+    },
+    { 
+      photo:"https://www.firstpost.com/wp-content/uploads/2022/02/India-America-1.jpg?impolicy=website&width=640&height=362",
+      h1tag:"Union Budget 2022: Tax regime should not behave like tax terrorism but like tax partners, says Mukesh Aghi",
+      ptag:"The president of the US India Strategic and Partnership Forum said American companies are keen on investing in India as they believe that the fundamentals of the economy remain strong in the country."
+
+    },
+    { 
+      photo:"https://images.firstpost.com/wp-content/uploads/2021/07/monsoon-session-parliament-640.jpg?impolicy=website&width=640&height=362",
+      h1tag:"‘Consensus” at all party meeting to not stall Houses, says Centre amid Pegasus row",
+      ptag:"This comes after the Pegasus row had led to a washout of the last Monsoon session when a united Opposition did not allow both Houses to function and sought a discussion on the matter"
+
+    },
+]
+
+
+
+let chaildSy="chilSy"
+let link= "./health.html"
+
+dataSy.forEach(function (el, index, arr) {
+
+    let avtar = document.createElement("img")
+
+    avtar.setAttribute("src",el.photo)
+
+    let imdiv=document.createElement("div")
+    imdiv.setAttribute("class" , chaildSy)
+
+    let A=document.createElement("a")
+    A.setAttribute("href" , link)
+    let h1=document.createElement("h1")
+    let p=document.createElement("p")
+   
+    h1.innerText = el.h1tag
+    p.innerText = el.ptag
+
+    // h3.innerText=""
+    A.innerText="HEALTH"
+
+    
+    imdiv.append(A,h1,p)
+    document.querySelector("#mainSy").append(avtar,imdiv)
+
+
+})
+
+
+
+
+console.log(document)
