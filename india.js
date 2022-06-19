5988248c361b7c511b819720ed7d9468293fb33d
Ujjwal singh <ujjwalsingh13feb@gmail.com>
2022-06-19 06:10:18 +0530

    final_page_submission

---
+++ india.js
@@ -0,0 +1,167 @@
+let dataSy = [
+    {
+      photo:
+        "https://images.firstpost.com/wp-content/uploads/2022/06/wildlife-2.jpg?impolicy=website&width=640&height=362",
+      h1tag:
+        "Centre to move SC on issue of ESZ around protected forests, says Ashwini Kumar Choubey",
+      ptag: "The apex court recently issued a direction that each protected forest, including national parks and wildlife sanctuaries, must have an ESZ of one kilometre and banned mining activities within such parks across the nation",
+    },
+    {
+      photo:
+        "https://images.firstpost.com/wp-content/uploads/2022/06/nupursharma.jpg?impolicy=website&width=640&height=362",
+      h1tag:
+        "Nupur Sharma, neo-feudalism and the geopolitical squeeze on India",
+      ptag: "As John Mearsheimer candidly admits, the US blundered in allowing China to escape from serfdom. But it is not too late to contain India! In this, the US and China are one: There is no way India can be allowed to gain power",
+    },
+    {
+      photo:
+        "https://images.firstpost.com/wp-content/uploads/2022/05/Terrorist-killed-in-Jammu.jpg?impolicy=website&width=640&height=362",
+      h1tag:
+        "Police officer shot dead in Jammu and Kashmir's Pulwama",
+      ptag: "Sub-Inspector Farooq Ahmad Mir's body was found in the paddy field near his home. Officials said Mir, a resident of Samboora in Pampore area of the south Kashmir district, was shot dead by terrorists",
+    },
+    {
+      photo:
+        "https://images.firstpost.com/wp-content/uploads/2022/06/Shiv-Sena.jpg?impolicy=website&width=640&height=362",
+      h1tag:
+        "People may ask how Opposition will give capable PM if it can't field strong candidate for presidential polls: Shiv Sena",
+      ptag: "The term of President Ram Nath Kovind ends on 24 July and an election to find his successor is scheduled to be held on 18 July. The process to file nominations for the Presidential polls began on 15 June",
+    },
+    {
+      photo:
+        "https://images.firstpost.com/wp-content/uploads/2022/06/rsz_smriti_irani_cnn_news18.jpg?impolicy=website&width=640&height=362",
+      h1tag:
+        "CNN-News18 Town Hall: 'India now has a PM who wants women to be equal partners in nation's growth,' says Smriti Irani",
+      ptag: "'Women help desks became a reality because of the Modi government. More than 700 fast track courts have been set up by this government and 35 helplines dedicated to women were introduced,' the union minister said",
+    },
+    {
+      photo:
+        "https://images.firstpost.com/wp-content/uploads/2022/06/External-Affairs-Minister-S.-Jaishankar.jpg?impolicy=website&width=640&height=362",
+      h1tag:
+        "Agnipath protests: Aspirants are being misled, says Union Minister Anurag Thakur",
+      ptag: "Some people are trying to do politics on Agnipath scheme, said Anurag Thakur at CNN-News18 Town Hall",
+    },
+    {
+      photo:
+        "https://images.firstpost.com/wp-content/uploads/2022/06/fake-officials1.jpg?impolicy=website&width=640&height=362",
+      h1tag:
+        "Prophet remarks row: Countries appreciated it was not the government's stand, says Jaishankar",
+      ptag: "While speaking on the row at the CNN-News18 Townhall, the external affairs minister said sensibilities and sensitivities of people were impacted which the countries articulated but they did appreciate that this was not the position of the government",
+    },
+    {
+      photo:
+        "https://images.firstpost.com/wp-content/uploads/2022/06/WhatsApp-Image-2022-06-18-at-9.2-1.jpg?impolicy=website&width=640&height=362",
+      h1tag:
+        "UP: Power Minister swings into action against 'fake' electricity dept officials",
+      ptag: "A K Sharma said that if such people will not stop doing this, then strict action will be taken against them in future also",
+    },
+    {
+      photo:
+        "https://images.firstpost.com/wp-content/uploads/2022/06/rsz_luckeesarai_junction_bihar_agnipath.jpg?impolicy=website&width=640&height=362",
+      h1tag:
+        "US support for Pakistan contributed to India-Pak problems, says Jaishankar",
+      ptag: "The foreign minister was speaking at the CNN-News18 Townhall organised on Saturday",
+    },
+    {
+      photo:
+        "https://images.firstpost.com/wp-content/uploads/2022/06/Agniveers1.jpg?impolicy=website&width=640&height=362",
+      h1tag:
+        "Agnipath stir: Bihar ruling allies BJP, JD(U) spar as Centre provides security to leaders",
+      ptag: "On Saturday, Bihar BJP chief Sanjay Jaiswal accused the administration led by CM Nitish Kumar of inciting violence against the saffron party in the state",
+    },
+    {
+      photo:
+        "https://images.firstpost.com/wp-content/uploads/2022/06/Partap-Singh-Bajwa1.jpg?impolicy=website&width=640&height=362",
+      h1tag:
+        "Agnipath Scheme: Govt lists support measures for Agniveers, check out details",
+      ptag: "States like Assam, Uttar Pradesh, Madhya Pradesh, Haryana, Arunachal Pradesh, Uttarakhand and Karnataka have already announced supportive measures for the Agniveers post-retirement from services after four years in the Armed Forces",
+    },
+    {
+      photo:
+        "https://images.firstpost.com/wp-content/uploads/2022/06/himanta-2.jpg?impolicy=website&width=640&height=362",
+      h1tag:
+        "Partap Singh Bajwa seeks resolution against Agnipath scheme in Punjab Assembly",
+      ptag: "Partap Bajwa, who is the Leader of Opposition in the Punjab Assembly, also requested the state government to take an all-party delegation to meet the prime minister over the Agnipath scheme",
+    },
+    {
+      photo:
+        "https://images.firstpost.com/wp-content/uploads/2022/06/Gadkari13.jpg?impolicy=website&width=640&height=362",
+      h1tag:
+        "Assam floods: PM Modi calls up Himanta Biswa Sarma; CM visits relief camps",
+      ptag: "The chief minister visited flood-affected Rangia in Kamrup district, and reviewed the situation with state BJP chief and local MLA Bhabesh Kalita. Sarma also visited relief camps at Fatima Convent School and Kolajal",
+    },
+    {
+      photo:
+        "https://images.firstpost.com/wp-content/uploads/2022/06/Nana-Patole1.jpg?impolicy=website&width=640&height=362",
+      h1tag:
+        "Nitin Gadkari says his ministry set five world records this year, hails hard work of stakeholders",
+      ptag: "Gadkari said the credit for all this does not go to him, but to the engineers, contractors, consultants, workers who toiled through day and night",
+    },
+    {
+      photo:
+        "https://images.firstpost.com/wp-content/uploads/2022/06/rsz_1arnold.jpg?impolicy=website&width=640&height=362",
+      h1tag:
+        "Anand Mahindra’s hilarious tweet about Arnold Schwarzenegger with a desi twist blows away internet",
+      ptag: "Tagging the wall art as ‘The Great Indian Funnel’, Mahindra wrote in the caption, “We adopt everything and everyone & give them Indian Avatars",
+    },
+    {
+      photo:
+        "https://images.firstpost.com/wp-content/uploads/2022/06/rsz_pm_in_vadodara.jpg?impolicy=website&width=640&height=362",
+      h1tag: "PM Modi lays foundation of developmental projects worth Rs 21,000 crore in Gujarat",
+      ptag: "The PM laid the foundation stone for the redevelopment of Surat, Udhna, Somnath and Sabarmati stations",
+    },
+    {
+      photo:
+        "https://images.firstpost.com/wp-content/uploads/2022/06/Mahendra_Nath01_J7Xt6jU-1.jpg?impolicy=website&width=640&height=362",
+      h1tag: "PM Modi lays foundation of developmental projects worth Rs 21,000 crore in Gujarat",
+      ptag: "The PM laid the foundation stone for the redevelopment of Surat, Udhna, Somnath and Sabarmati stations",
+    },
+    {
+      photo:
+        "https://images.firstpost.com/wp-content/uploads/2022/06/WhatsApp-Image-2022-06-18-at-6.2-1.jpg?impolicy=website&width=640&height=362",
+      h1tag:
+        "Tesla, Elon Musk welcome, but India will not compromise on policy, says Mahendra Nath Pandey",
+      ptag: "Last month, Musk had said Tesla will not manufacture its products locally unless it is allowed to first sell and service its cars in the country",
+    },
+    {
+      photo:
+        "https://www.firstpost.com/wp-content/uploads/2022/06/rsz_bihar_train_agnipath.jpg?impolicy=website&width=640&height=362",
+      h1tag:
+        "Bihar: Train services suspended as violence over Agnipath scheme continues, role of coaching centres being probed",
+      ptag: "'We have found out about the role of 7-8 coaching centres through WhatsApp messages from those arrested,' a top official in Patna said",
+    },
+    {
+      photo:
+        "https://images.firstpost.com/wp-content/uploads/2022/06/Kumawat1.jpg?impolicy=website&width=640&height=362",
+      h1tag:
+        "Rajasthan's Kumawat community prohibits grooms with beard from getting married, here's why",
+      ptag: "About 20,000 people from the 19 villages of the Kumawat community have shifted to different cities of Maharashtra, Gujarat and southern states and they will also be required to follow the rules even if they performed rituals in the cities they are living in",
+    },
+  ];
+  
+  let chaildSy = "chilSy";
+  let link = "india.html";
+  
+  dataSy.forEach(function (el, index, arr) {
+    let avtar = document.createElement("img");
+  
+    avtar.setAttribute("src", el.photo);
+  
+    let imdiv = document.createElement("div");
+    imdiv.setAttribute("class", chaildSy);
+  
+    let A = document.createElement("a");
+    A.setAttribute("href", link);
+    let h1 = document.createElement("h1");
+    let p = document.createElement("p");
+  
+    h1.innerText = el.h1tag;
+    p.innerText = el.ptag;
+  
+    // h3.innerText=""
+    A.innerText = "INDIA";
+  
+    imdiv.append(A, h1, p);
+    document.querySelector("#mainSy").append(avtar, imdiv);
+  });
+  console.log(document);
